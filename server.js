147f0ded569fb2a4d4ca2a670d61973365694827
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2023-09-26 20:49:37 -0400

    Updated Server to listen on env PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const DefaultAzureCredential = require('@azure/identity');
 const { PrismaClient } = require('@prisma/client');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const uploadStrategy = require('./upload');
 const azureUpload = require('./azure');
